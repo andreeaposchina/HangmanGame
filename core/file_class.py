c3eecd0eee365404fe5caff83aef8a68a628031b
Andreea Poschina <poschinaandreea@yahoo.com>
2021-11-23 22:50:18 +0200

    docstring added

--- core/file_class.py
+++ core/file_class.py
@@ -3,30 +3,54 @@ from random import choice
 from os import path
 
 
-class File:
+class Record:
     def __init__(self, filename):
         self.filename = filename
 
     def read_lines(self) -> list:
+        """
+        This method is used to read the content of a file by lines.
+        :return: A list of lists that contains the lines of the file
+        """
         with open(self.filename) as file:
             lines = file.readlines()
             lines = [line.rstrip() for line in lines]
         return lines
 
     def read_file(self) -> str:
+        """
+        This method is used to read the content of a file.
+        :return: A string that contains the content of the file
+        """
         with open(self.filename, 'r') as file:
             data = file.read()
         return data
 
-    def write_file(self, data) -> None:
+    def write_file(self, data: str) -> None:
+        """
+        This method is used to write data into a file.
+        :param data: Must be a string
+        :return: This method does not return something.
+        """
         with open(self.filename, 'w') as file:
             file.write(data)
 
     def choose_word(self) -> str:
+        """
+        This method is used to choose a random word from a given file
+        :return: It returns string which represents the word
+        """
         lst = self.read_lines()
         return choice(lst)
 
     def build_history(self, data: str):
+        """
+        This method is used to keep track of the history of players and their progress.
+        If the file exists, then this method will overwrite the existing file. If not, then the file will be created,
+        and the data will be written.
+        :param data: Must be a string
+        :return: This method does not return something.
+        """
         if path.isfile(BASE_DIR_PATH + "\\" + self.filename):
             existing_data = self.read_file()
             data = existing_data + data
@@ -35,6 +59,11 @@ class File:
             self.write_file(data)
 
     def build_ranking(self):
+        """
+        This method is used to sort the data within the file that contains the progress of
+        players in order to create a ranking.
+        :return: This method does not return something.
+        """
         data = self.read_lines()
         data_list = [elem.split(',') for elem in data]
         data_list = [[elem.strip() for elem in sublist] for sublist in data_list]
--- core/hangman_class.py
+++ core/hangman_class.py
@@ -1,11 +1,11 @@
-from core.file_class import File
+from core.file_class import Record
 from datetime import datetime
 
 
 class Hangman:
     def __init__(self):
-        self.words_file_instance = File("words_file.txt")
-        self.history_file_instance = File("history_points.txt")
+        self.words_file_instance = Record("words_file.txt")
+        self.history_file_instance = Record("history_points.txt")
         self.word = self.words_file_instance.choose_word()
         self.name = ''
         self.no_points = 0
@@ -18,18 +18,11 @@ class Hangman:
             self.start = input("Wrong command. "
                                "Please press Y and ENTER if you want to play or N and ENTER if you want to exit: ")
 
-    def play_game(self):
-        if self.start.lower() == 'y':
-            self._start_game()
-        else:
-            self._exit_game()
-
-        self._calculate_points()
-        self._add_user_to_history()
-        self._show_results()
-        self._print_ranking()
-
     def _start_game(self):
+        """
+        This method contains the main algorithm of the Hangman game.
+        :return: This method does not return something.
+        """
         self.name = input("Please enter your name or nickname: ").upper()
         guessed_letters = []
         given_word_list = [letter for letter in self.word]
@@ -57,25 +50,61 @@ class Hangman:
 
     @staticmethod
     def _exit_game():
+        """
+        This method is used to exit the game if the user does not want to play.
+        :return: This method does not return something.
+        """
         print("Ok, bye :(")
         exit()
 
     def _calculate_points(self):
+        """
+        This method is used to compute the total number of points.
+        The final number of points depends on the length of the word and on the number of lives that remained.
+        :return: This method does not return something
+        """
         self.no_points = 100 * self.no_lives * len(self.word)
 
     def _show_results(self):
+        """
+        This method is always returned at the end of a game. It prints a summary of the game.
+        :return: This method does not return something
+        """
         if self.no_lives == 0:
             print(f"Game over, {self.name}! :( The word was {self.word} \n "
                   f"You obtained {self.no_points} points \n Better luck next time!")
         else:
-            print(f"Congratulations, {self.name}! You obtained {self.no_points} points")
+            print(f"Congratulations, {self.name}! :) You obtained {self.no_points} points")
 
     def _add_user_to_history(self):
+        """
+        This method is used to record the points, the user and the date and time when the user got the score.
+        :return: This method does not return something
+        """
         data = f"{self.name}, {self.no_points}, {datetime.now().strftime('%d/%m/%Y %H:%M:%S')} \n"
         self.history_file_instance.build_history(data)
 
     def _print_ranking(self):
+        """
+        This method is always returned at the end of a game. It prints the history of plays by making a ranking.
+        :return: This method does not return something
+        """
         self.history_file_instance.build_ranking()
         ranking = self.history_file_instance.read_lines()
         for line in ranking:
             print(line)
+
+    def play_game(self):
+        """
+        This is the main method. It calls all methods from the class.
+        :return: This method does not return something
+        """
+        if self.start.lower() == 'y':
+            self._start_game()
+        else:
+            self._exit_game()
+
+        self._calculate_points()
+        self._add_user_to_history()
+        self._show_results()
+        self._print_ranking()
